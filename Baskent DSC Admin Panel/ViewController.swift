e725ce75f5c278417c4bf18944f9b3e199f7ad76
DorukOzdemir06 <dorukozdemir06@outlook.com>
2023-01-06 19:21:14 +0300

    Initial Commit

---
+++ Baskent DSC Admin Panel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baskent DSC Admin Panel
+//
+//  Created by Doruk Özdemir on 6.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
